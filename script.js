d32aca05c31efb83c7ebfef506bb43754d8462f9
Brennan Smallwood <39069788+BSmalls6@users.noreply.github.com>
2019-10-14 16:56:56 -0700

    added scorecard

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// 1. create start card, with start quiz button
 $(document).ready(function () {
+    // My questions in an array
     var myQuestions = [
         {
+            // individual questions are an object in the array
             question: "Who was the first Avenger?",
+            // answers are an array within the question object
             answers: [
                 "Steve Rogers",
                 "Iron Man",
                 "Captain Marvel",
                 "Thor"
             ],
+            // correct answer is object within question object
             correctAnswer: 0
         },
         {
@@ -52,25 +55,30 @@ $(document).ready(function () {
             correctAnswer: 3
         }
     ];
-    // i. start quiz button displays first question
+    // Sets the score and the question # to 0
     var i = 0;
     var score = 0;
-
+// Click Events for each button
+    // start starts the quiz and goes until all 5 questions have been answered
     $("#start").on("click", startQuiz);
+    // reset saves the score and reloads the quiz
     $("#reset").on("click", function(){
+        event.preventDefault();
+        saveScore();
+        alert("Your Score has been saved")
         location.reload();
-    });
+        });
+        // results shows the highscores
+    $("#results").on("click", highScores);
 
-    // $("#reset").on("click", startQuiz);
-    // ii. when first question displays, timer starts counting down
 
-    // 2. build quiz
+    // 2. start quiz, hides all html except the quiz and the header elements, starts the timer
     function startQuiz() {
-        // event.preventDefault();
         timer();
         $(".start").hide();
         $(".qSlide").show();
         $(".endSlide").hide();
+        $(".scorePage").hide();
         buildQuiz();
     };
 
@@ -93,17 +101,23 @@ $(document).ready(function () {
                 timeLeft--;
             }
         };
-    }
+    };
+    // Answer checker - adds click listener to answer buttons, 
+
     $(".choices").on("click", '#choice', function () {
         var selection = parseInt($(this).attr("value"));
         // console.log(typeof selection);
         // console.log(myQuestions[i].correctAnswer);
+            // checks if answer is right or wrong
         if (selection === myQuestions[i].correctAnswer) {
+            // adds +1 to the score variable
             score++;
             $(".grade").html("Correct!")
+            // adds +1 to the question, so that it displays the next question in the array
             i++;
+            // clears the section to display the next question
             $(".choices").empty();
-            // console.log("correct", i)
+            // checks what question we are on, if we complete the last question,use different function
             if (i <= 4) {
                 buildQuiz();
             }
@@ -130,58 +144,53 @@ $(document).ready(function () {
     });
 
     function buildQuiz() {
+        // grabs the current question based on the value of i
         var currentQuestion = myQuestions[i];
-        var lastQ = myQuestions[4];
+        // displays the current question in the div
         $(".question").html(currentQuestion.question);
+    //    loops through the answers, creates a div for each one
         for (let j = 0; j < 4; j++) {
             var choicebtn = $("<div id='choice'></div>")
             var currentAns = currentQuestion.answers[j];
+            // adds classes to the divs, makes them buttons
             choicebtn.addClass("btn btn-primary");
             choicebtn.attr("value", j);
+        //    adds each button next to each other
             $(".choices").append(choicebtn);
+            // changes the text content of each button to the answer, relative to j value
             choicebtn.html(currentAns);
             // console.log(currentAns)          // if answer = correct answer - show correct, else show wrong
         }
 
     };
+    // displays the ending screen
     function showScore() {
+        // hides the question div
         $(".qSlide").hide();
+        // shows the ending screen
         $(".endSlide").show();
+        // shows your score within the score span, which is within a text div in the HTML
         $("#displayScore").text(score);
+    };
+    // saves the score to local storage, resets the quiz
+    function saveScore(){
+        var nameInput = document.getElementById('name');
+
+        $('#reset').on('click', function (e) {
+        
+            //prevent the normal submission of the form
+            e.preventDefault();
+        localStorage.setItem('newScore',nameInput.value+' '+score)
+            console.log(nameInput.value+" "+score);    
+        });
+    };
+    function highScores(){
+        $("scorePage").show();
+        $(".start").hide();
+        $(".qSlide").hide();
+        $(".endSlide").hide();
+        localStorage.getItem('newScore')
+        $('scoreTable').append(newScore);
     };
 
 });
-
-function showScore() {
-    $(".qSlide").hide();
-    $(".endSlide").show();
-    $("#displayScore").text(score);
-};
-
-// function test () {
-//     var selection = parseInt(this.value);
-//     console.log(selection);
-//     console.log(myQuestions[i].correctAnswer);
-
-//     if (selection === myQuestions[i].correctAnswer) {
-//         score++;
-//         $(".question").append("Correct!")
-//         i++;
-//         buildQuiz();
-//     }
-//     else {
-//         timeleft = -15
-//         $(".question").append("Wrong")
-//         i++;
-//         buildQuiz();
-//     }
-// }
-
-    // if answer = correct answer - show green, else show red
-    // get correct answer
-// 4. clicking on each question displays either correct or incoorect
-    // i. right answer click = sound #1
-    // ii. wrong answer click = sound #2
-// 5. when an answer is clicked, next question is displayed
-// 6. when all questions have been answered, it displays your score and your ranking on the leaderboard
-// 7. when leaderboard screen is displayed, there is a button that allows the user to start the quikz over again
